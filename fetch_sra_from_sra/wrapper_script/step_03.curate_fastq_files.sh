08f5480111ac84ffd06a6db3ae31542453322fe5
yang-kaist <45616714+yang-kaist@users.noreply.github.com>
2026-09-15 17:27:26 +0900

    Create step_03.curate_fastq_files.sh

---
+++ fetch_sra_from_sra/wrapper_script/step_03.curate_fastq_files.sh
@@ -0,0 +1,19 @@
+  mkdir -p ../ATAC_read/hg38/D7_S1/ ; mv ../tmp/SRR22483926/SRR22483926_1.fastq.gz ../ATAC_read/hg38/D4_S1/I2.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S1/ ; mv ../tmp/SRR22483926/SRR22483926_2.fastq.gz ../ATAC_read/hg38/D4_S1/I1.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S1/ ; mv ../tmp/SRR22483926/SRR22483926_3.fastq.gz ../ATAC_read/hg38/D4_S1/R1.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S1/ ; mv ../tmp/SRR22483926/SRR22483926_4.fastq.gz ../ATAC_read/hg38/D4_S1/R2.fastq.gz
+  
+  mkdir -p ../ATAC_read/hg38/D7_S4/ ; mv ../tmp/SRR22483934/SRR22483934_1.fastq.gz ../ATAC_read/hg38/D7_S1/I2.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S4/ ; mv ../tmp/SRR22483934/SRR22483934_2.fastq.gz ../ATAC_read/hg38/D7_S1/I1.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S4/ ; mv ../tmp/SRR22483934/SRR22483934_3.fastq.gz ../ATAC_read/hg38/D7_S1/R1.fastq.gz
+  mkdir -p ../ATAC_read/hg38/D7_S4/ ; mv ../tmp/SRR22483934/SRR22483934_4.fastq.gz ../ATAC_read/hg38/D7_S1/R2.fastq.gz
+  
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483942/SRR22483942_1.fastq.gz ../RNA_read/hg38/D4_S1/I2.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483942/SRR22483942_2.fastq.gz ../RNA_read/hg38/D4_S1/I1.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483942/SRR22483942_3.fastq.gz ../RNA_read/hg38/D4_S1/R1.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483942/SRR22483942_4.fastq.gz ../RNA_read/hg38/D4_S1/R2.fastq.gz
+  
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483950/SRR22483950_1.fastq.gz ../RNA_read/hg38/D7_S1/I2.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483950/SRR22483950_2.fastq.gz ../RNA_read/hg38/D7_S1/I1.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483950/SRR22483950_3.fastq.gz ../RNA_read/hg38/D7_S1/R1.fastq.gz
+  mkdir -p ../RNA_read/hg38/D7_S1/  ; mv ../tmp/SRR22483950/SRR22483950_4.fastq.gz ../RNA_read/hg38/D7_S1/R2.fastq.gz
