0ceb6afab342ce46d1ae1636caebbad3e4af6d0b
yang-kaist <45616714+yang-kaist@users.noreply.github.com>
2026-09-15 17:11:34 +0900

    Create step_01.fasterq-dump.sh

---
+++ fetch_sra_from_sra/wrapper_script/step_01.fasterq-dump.sh
@@ -0,0 +1,4 @@
+ singularity exec ../singularity/sra-tools.sif fasterq-dump --split-files --include-technical --threads 8 -O ../tmp/SRR22483934 SRR22483934 # GSM6706649: 201218_ATAC_D7_S1
+ singularity exec ../singularity/sra-tools.sif fasterq-dump --split-files --include-technical --threads 8 -O ../tmp/SRR22483937 SRR22483937 # GSM6706652: 201218_ATAC_D7_S4
+ singularity exec ../singularity/sra-tools.sif fasterq-dump --split-files --include-technical --threads 8 -O ../tmp/SRR22483950 SRR22483950 # GSM6706665: 201218_RNA_D7_S1
+ singularity exec ../singularity/sra-tools.sif fasterq-dump --split-files --include-technical --threads 8 -O ../tmp/SRR22483953 SRR22483953 # GSM6706668: 201218_RNA_D7_S4
